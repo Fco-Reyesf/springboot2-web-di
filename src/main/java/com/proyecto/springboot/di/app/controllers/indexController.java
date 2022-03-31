c45541d93103240517b3a547f4f5e3c5ce1f3695
Fco-Reyesf <freyes.info@gmail.com>
2022-03-30 21:31:19 -0300

    metodos de inyeccion

--- src/main/java/com/proyecto/springboot/di/app/controllers/indexController.java
+++ src/main/java/com/proyecto/springboot/di/app/controllers/indexController.java
@@ -11,19 +11,45 @@ import com.proyecto.springboot.di.app.models.service.IService;
  * DAO : data access object. accede a datos realizando consultas.
  * controlador interactua con los dao (service)
  * dependencia, no crea instancia, se crea por si sola (aparte)
+ * @repository: DAOs
+ * @service: logica de negocio y datos
+ * beans es como un componente, @autowired obliga a tener un constructor vacio.
  */
 
 @Controller
 public class indexController {
 	
-	// crea la inyeccion 
-	@Autowired
+	/*
+	 * @autowired crea la inyeccion
+	 * puede colocarse en la instancia del servicio o en los set o get
+	 * pero solo puede ser en uno solo
+	 */
+	
+	@Autowired    // opcion 1
 	private IService servicio;
 	
+	
+	// cuando se inyecta por constructor se puede omitir la sentencia @autowired
+	/*
+	@Autowired
+	public indexController(IService servicio) {
+		this.servicio = servicio;
+	}
+	*/
+
+
 	@GetMapping({"","/","/index"})
 	public String index(Model modelo) {
 		modelo.addAttribute("dataServicio", servicio.operacion());
 		return "index";
 	}
+
+	/* opcion con los set y get
+	@Autowired
+	public void setServicio(IService servicio) {
+		this.servicio = servicio;
+	}
+	*/
+	
 	
 }
--- src/main/java/com/proyecto/springboot/di/app/models/service/service.java
+++ src/main/java/com/proyecto/springboot/di/app/models/service/service.java
@@ -8,6 +8,8 @@ public class service implements IService {
 	 /*
 	  * todo componente debe tener su constructor vacio
 	  * porque utiliza el constructor para crear el componente.
+	  * inyeccion de dependencia, es entregar la referencia de un objeto a otro.
+	  * no lo llama, si no que entrega los servicios.
 	  */
 	
 	public service() {}
